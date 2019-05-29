324658fa1d5b74dcd26ddb8eac2303553caa66bd
ijelynsaele.com <ijelyn@saele.com>
2019-05-29 10:42:32 +0200

    my farge spill not finish

---
+++ script.js
@@ -0,0 +1,8 @@
+//document.g.style.backgroundColor = "red";
+//document.getElementById("message").style.backgroundColor = "rgb(255,150,0)";
+//var myButtons = document.getElementsByClassName("buttons")[0];
+//myButtons.style.backgroundColor = "red";
+//console.log("buttons =", myButtons);
+function ramdomColor() { 
+    var ramdomColor = "RGB";
+    
